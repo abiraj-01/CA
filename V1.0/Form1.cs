24e7880e2859cd6b904ec3e3deede82704eaa044
abiraj-01 <yoghanraj1002@gmail.com>
2025-05-23 09:00:31 +0530

    Final improved version v2.0

--- V1.0/Form1.cs
+++ V1.0/Form1.cs
@@ -39,5 +39,24 @@ namespace V1._0
             txtDueDate.Clear();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtTask.Clear();
+            txtDueDate.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (listBoxTasks.SelectedIndex != -1)
+            {
+                listBoxTasks.Items.RemoveAt(listBoxTasks.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.", "Delete Task", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
     }
+        
 }
